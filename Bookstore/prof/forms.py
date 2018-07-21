26cdbb0855108c58d45820cfc88def85d39ffdd6
Jorge Costafreda <jorge.costafreda@yahoo.com>
2018-07-21 10:54:56 -0400

    Jessicas work: Added modify password and can now add shipping addresses to user accounts

--- Bookstore/prof/forms.py
+++ Bookstore/prof/forms.py
@@ -5,11 +5,13 @@ class RegistrationForm(forms.Form):
     Last_Name = forms.CharField(label = 'Last Name', max_length= 100)
     Username = forms.CharField(label='Username', max_length=100)
     Email = forms.EmailField(label ='Email')
-    Address =forms.CharField(label = 'Address(Street)', max_length=60)
-    City = forms.CharField(label='Address(City)', max_length=50)
-    Zip_Code =forms.IntegerField(label='Zip Code')
     psw = forms.CharField(widget=forms.PasswordInput, label='Password')
     psw_repeat = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+
+class ChangePassword(forms.Form):
+    old_psw = forms.CharField(widget=forms.PasswordInput, label='old_psw')
+    psw = forms.CharField(widget=forms.PasswordInput, label='psw')
+    psw_confirm = forms.CharField (widget=forms.PasswordInput, label='psw_confirm')
 
 class LoginForm(forms.Form):
     Username = forms.CharField(label='Username', max_length=100)
@@ -23,6 +25,15 @@ class AddReservedCardForm(forms.Form):
     Fname =  forms.CharField(max_length=32)
     Lname = forms.CharField(max_length=32)
     Mname = forms.CharField(max_length=32)
+
+class AddUserAddress(forms.Form):
+    Address_1 = forms.CharField(max_length=60)
+    Address_2 = forms.CharField (max_length =60)
+    City = forms.CharField(max_length=50)
+    State = forms.CharField (max_length=50)
+    Zip_Code = forms.IntegerField()
+    Name = forms.CharField (max_length=50)
+    Country = forms.CharField (max_length=50)
 
 class AddPreferredCardForm(forms.Form):
     C_card_number = forms.CharField(max_length = 24)
--- Bookstore/prof/urls.py
+++ Bookstore/prof/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path ('profileInfo/', views.profileInfo, name = 'profileInfo'),
     path('register/', views.RegistrationView, name = 'register'),
     path('login/', views.user_login, name = 'login'),
+    path('login/error/', views.user_login, name = 'login_error'),
+    path('change_password/', views.change_password, name ="change_password"),
     path('logout/', views.user_logout, name = 'logout'),
     path('add_reserved_card/', views.add_reserved_card, name = "add_reserved_card"),
     path('add_preferred_card/', views.add_preferred_card, name = "add_preferred_card"),
+    path('add_address/', views.add_shipping_address, name = "add_shipping_address"),
+    path('modify_password/', views.modify_password, name ="modify_password")
 ]
